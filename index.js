03af3a85299073a901e07b99f8a1e9ee9dbde31f
Izz Abdallah <x3zoabdullah@gmail.com>
2024-05-20 16:05:15 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+humanScore=0
+computerScore=0
+Round=1
+
+
+
+function getComputerChoice(){
+    Choices=["Rock","Paper","Scissors"]
+    return Choices[parseInt(Math.random()*3)].toUpperCase()
+
+}
+
+function getHumanChoice(){
+   return prompt("Select: Rock,Paper,Scissors").toUpperCase()
+}
+
+
+function playRound(){
+    let humanChoice=getHumanChoice()
+    let computerChoice=getComputerChoice()
+    console.log(`ROUND: ${Round}`)
+    Round+=1
+    if(humanChoice===computerChoice){
+        console.log(`human choice: ${humanChoice}`)
+        console.log(`computer choice: ${computerChoice}`)
+        console.log(`It's a TIE!\nHuman Score: ${humanScore}     Computer Score: ${computerScore}` )
+    }
+
+    else if(humanChoice==="ROCK" && computerChoice=="PAPER"){
+        console.log(`human choice: ${humanChoice}`)
+        console.log(`computer choice: ${computerChoice}`)
+        computerScore+=1
+        console.log(`COMPUTER WIN!\nHuman Score: ${humanScore}    Computer Score: ${computerScore}` )
+    }
+    else if(humanChoice==="PAPER" && computerChoice=="SCISSORS"){
+        console.log(`human choice: ${humanChoice}`)
+        console.log(`computer choice: ${computerChoice}`)
+        computerScore+=1
+        console.log(`COMPUTER WIN!\nHuman Score: ${humanScore}     Computer Score: ${computerScore}` )
+    }
+    else if(humanChoice==="SCISSORS" && computerChoice=="ROCK"){
+        console.log(`human choice: ${humanChoice}`)
+        console.log(`computer choice: ${computerChoice}`)
+        computerScore+=1
+        console.log(`COMPUTER WIN!\nHuman Score: ${humanScore}     Computer Score: ${computerScore}` )
+    }
+    else{
+        console.log(`human choice: ${humanChoice}`)
+        console.log(`computer choice: ${computerChoice}`)
+        humanScore+=1
+        console.log(`HUMAN WIN!\nHuman Score: ${humanScore}    Computer Score: ${computerScore}` )
+    }
+
+    if(Round>5){
+        if(humanScore>computerScore){
+            console.log(`--------------------------------\nHUMAN IS THE WINNER!`)
+        }
+        else if(computerScore>humanScore){
+            console.log(`--------------------------------\nCOMPUTER IS THE WINNER`)
+        }
+        else{
+            console.log("ITS A TIE!")
+        }
+    }
+
+}
+
+function playGame(){
+    playRound()
+    playRound()
+    playRound()
+    playRound()
+    playRound()
+}
+
+
+
+playGame()
+
+
+ 
+
+ 
