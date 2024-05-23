7c01004721f340eca5062204d8837adcbe25399b
Izz Abdallah <x3zoabdullah@gmail.com>
2024-05-23 11:31:00 +0400

    Changes

--- index.js
+++ index.js
@@ -1,82 +1,75 @@
+const playerChoice = document.querySelector(".Player")
+
+const computerChoice = document.querySelector(".Computer")
+
+const Result = document.querySelector(".Result")
+
+
+const Choices=["ROCK","PAPER","SCISSORS"]
+
+
+const HUMANScore=document.querySelector(".ComputerScore")
+
+const COMPUTERScore=document.querySelector(".HumanScore")
+
+
+
 humanScore=0
-computerScore=0
-Round=1
+
+comptuerScore=0
 
 
 
-function getComputerChoice(){
-    Choices=["Rock","Paper","Scissors"]
-    return Choices[parseInt(Math.random()*3)].toUpperCase()
+function Game(playerChoiceFromClickedButton){
 
+    Result.textContent=""
+
+    computerChoicefromChoices=Choices[parseInt(Math.random()*3)]
+
+    playerChoice.textContent=`Player:   ${playerChoiceFromClickedButton}`
+    computerChoice.textContent=`Computer: ${computerChoicefromChoices}`
+
+ 
+
+    switch(true){
+        case playerChoiceFromClickedButton===computerChoicefromChoices:
+            break;
+
+        case playerChoiceFromClickedButton==="ROCK" && computerChoicefromChoices==="PAPER":
+            comptuerScore+=1
+            break;
+
+        case playerChoiceFromClickedButton==="PAPER" && computerChoicefromChoices==="SCISSORS":
+
+            comptuerScore+=1
+            break;
+
+        case playerChoiceFromClickedButton==="SCISSORS" && computerChoicefromChoices==="ROCK":
+            comptuerScore+=1
+            break;
+        
+        default:
+            humanScore+=1
+    }
+
+    HUMANScore.textContent=`Human Score: ${humanScore}`
+    COMPUTERScore.textContent=`Computer Score: ${comptuerScore}`
+    HUMANScore.classList.add("HumanWin")
+    COMPUTERScore.classList.add("ComputerWin")
+
+
+    if(humanScore==5){
+        Result.textContent="HUMAN WIN! (It's a first to reach 5 score points is the winner )"
+        humanScore=0
+        comptuerScore=0
+    }
+
+   if(comptuerScore==5){
+    Result.textContent="COMPUTER WIN! (First to reach 5 score points is the winner)"
+    humanScore=0
+    comptuerScore=0
+   }
 }
-
-function getHumanChoice(){
-   return prompt("Select: Rock,Paper,Scissors").toUpperCase()
-}
-
-
-function playRound(){
-    let humanChoice=getHumanChoice()
-    let computerChoice=getComputerChoice()
-    console.log(`ROUND: ${Round}`)
-    Round+=1
-    if(humanChoice===computerChoice){
-        console.log(`human choice: ${humanChoice}`)
-        console.log(`computer choice: ${computerChoice}`)
-        console.log(`It's a TIE!\nHuman Score: ${humanScore}     Computer Score: ${computerScore}` )
-    }
-
-    else if(humanChoice==="ROCK" && computerChoice=="PAPER"){
-        console.log(`human choice: ${humanChoice}`)
-        console.log(`computer choice: ${computerChoice}`)
-        computerScore+=1
-        console.log(`COMPUTER WIN!\nHuman Score: ${humanScore}    Computer Score: ${computerScore}` )
-    }
-    else if(humanChoice==="PAPER" && computerChoice=="SCISSORS"){
-        console.log(`human choice: ${humanChoice}`)
-        console.log(`computer choice: ${computerChoice}`)
-        computerScore+=1
-        console.log(`COMPUTER WIN!\nHuman Score: ${humanScore}     Computer Score: ${computerScore}` )
-    }
-    else if(humanChoice==="SCISSORS" && computerChoice=="ROCK"){
-        console.log(`human choice: ${humanChoice}`)
-        console.log(`computer choice: ${computerChoice}`)
-        computerScore+=1
-        console.log(`COMPUTER WIN!\nHuman Score: ${humanScore}     Computer Score: ${computerScore}` )
-    }
-    else{
-        console.log(`human choice: ${humanChoice}`)
-        console.log(`computer choice: ${computerChoice}`)
-        humanScore+=1
-        console.log(`HUMAN WIN!\nHuman Score: ${humanScore}    Computer Score: ${computerScore}\n--------------------------` )
-    }
-
-    if(Round>5){
-        console.log(`HUMAN SCORE: ${humanScore}         COMPUTER SCORE: ${computerScore}`)
-        if(humanScore>computerScore){
-            console.log(`--------------------------------\nHUMAN IS THE WINNER!`)
-        }
-        else if(computerScore>humanScore){
-            console.log(`--------------------------------\nCOMPUTER IS THE WINNER`)
-        }
-        else{
-            console.log("ITS A TIE!")
-        }
-    }
-
-}
-
-function playGame(){
-    playRound()
-    playRound()
-    playRound()
-    playRound()
-    playRound()
-}
-
-
-
-playGame()
 
 
  
